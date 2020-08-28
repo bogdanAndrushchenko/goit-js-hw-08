acd5e3656f51f68b29030c8bdbfcccba559d2f67
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2020-08-28 14:30:00 +0300

    create starts file

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * ******* Задание в файле README.md ********
+ */
+// import "gallery-items.js"
+import galleryItems from "./gallery-items.js";
+console.log(galleryItems[0].description);
